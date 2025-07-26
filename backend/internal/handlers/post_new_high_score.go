6a5dad4b0577e0077ffd1d3a645ce5f69942b41d
Warren Paschetto <wdpaschetto@gmail.com>
2025-07-26 14:16:42 -0400

    added update and cleaning of Top Ten leaderboard list

--- backend/internal/handlers/post_new_high_score.go
+++ backend/internal/handlers/post_new_high_score.go
@@ -16,7 +16,9 @@ type PostScoreRequest struct {
 }
 
 type Command interface {
-	InsertNewScore(ctx context.Context, arg db.InsertNewScoreParams) (db.Leaderboard, error)
+	InsertNewScore(ctx context.Context, arg db.InsertNewScoreParams) error
+	DeleteScoresAfterTen(ctx context.Context) error
+	OrganizeTopTenList(ctx context.Context) ([]db.Leaderboard, error)
 }
 
 func PostNewHighScore(q Command) http.HandlerFunc {
@@ -38,16 +40,35 @@ func PostNewHighScore(q Command) http.HandlerFunc {
 			Timer:    req.Timer,
 		}
 
-		result, err := q.InsertNewScore(r.Context(), arg)
-		if err != nil {
+		// Add new high score to top ten list
+
+		if err := q.InsertNewScore(r.Context(), arg); err != nil {
 			log.Printf("Error inserting new high score: %v", err)
 			http.Error(w, "Failed to save new high score", http.StatusInternalServerError)
 			return
 		}
 
+		// Delete extra scores and keep only 10
+		if err := q.DeleteScoresAfterTen(r.Context()); err != nil {
+			log.Printf("Failed to delete extra scores: %v", err)
+			http.Error(w, "Failed to trim leaderboard", http.StatusInternalServerError)
+			return
+		}
+
+		// Get updated leaderboard
+		updatedTopTen, err := q.OrganizeTopTenList(r.Context())
+		if err != nil {
+			log.Printf("Failed to update Top Ten list: %v", err)
+			http.Error(w, "Failed to update leaderboard", http.StatusInternalServerError)
+			return
+		}
+
 		w.Header().Set("Content-Type", "application/json")
-		if err := json.NewEncoder(w).Encode(result); err != nil {
+		w.WriteHeader(http.StatusCreated)
+		if err := json.NewEncoder(w).Encode(updatedTopTen); err != nil {
+			log.Printf("Failed to encode JSON: %v", err)
 			http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
+			return
 		}
 	}
 }
