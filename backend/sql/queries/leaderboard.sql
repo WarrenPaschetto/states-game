fbe23e9f1d2a43fe31e58dcd0c3a11934ba1b552
Warren Paschetto <wdpaschetto@gmail.com>
2025-07-26 07:48:24 -0400

    added GetLeaderboard handler

--- backend/sql/queries/leaderboard.sql
+++ backend/sql/queries/leaderboard.sql
@@ -2,7 +2,7 @@
 INSERT INTO leaderboard (user_name, score, timer)
 VALUES ($1, $2, $3);
 
--- name: OrganizeTopTenList :exec
+-- name: OrganizeTopTenList :many
 SELECT id, user_name, score, timer, created_at
 FROM leaderboard
 ORDER BY score DESC, timer ASC, created_at ASC
