9ed15d9d0acfaa5dfee20f1e8fd7038ac4d11bbf
Warren Paschetto <wdpaschetto@gmail.com>
2025-07-28 11:25:42 -0400

    Update 20250726082254_leaderboard.sql

--- backend/sql/schema/20250726082254_leaderboard.sql
+++ backend/sql/schema/20250726082254_leaderboard.sql
@@ -3,7 +3,7 @@ CREATE TABLE leaderboard (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_name TEXT NOT NULL,
     score INTEGER NOT NULL CHECK (score >= 0 AND score <= 50),
-    timer INTEGER NOT NULL CHECK (timer >= 0 AND timer <= 60),
+    timer INTEGER NOT NULL CHECK (timer >= 0 AND timer <= 240),
     created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
